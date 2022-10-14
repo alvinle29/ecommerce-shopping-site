aeaf9fa0462b113afe42177e9a2beda23d54d583
alvinle29 <bach938@gmail.com>
2022-10-14 16:13:48 +0300

    fetch first api worked

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import products from './data/Products.js'
+
+const app = express()
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+app.listen(5000, console.log("Server running on PORT 5000"))
