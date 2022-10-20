cf50b7a67da2e820d6f1f794db600aa61fece788
alvinle29 <bach938@gmail.com>
2022-10-21 00:30:36 +0300

    add redux on single product page

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,21 +1,20 @@
-import axios from "axios";
+import axios from "axios"
 import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-} from "../Constants/ProductConstants";
-import { logout } from "./userActions";
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+} from "../constants/productConstants"
 
-// PRODUCT LIST
-export const listProduct =
-  (keyword = " ", pageNumber = " ") =>
+export const listProduct = () =>
   async (dispatch) => {
     try {
-      dispatch({ type: PRODUCT_LIST_REQUEST });
-      const { data } = await axios.get(
-        `/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
-      );
-      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+      dispatch({ type: PRODUCT_LIST_REQUEST })
+      const { data } = await axios.get(`/api/products`)
+      console.log(data)
+      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     } catch (error) {
       dispatch({
         type: PRODUCT_LIST_FAIL,
@@ -23,6 +22,22 @@ export const listProduct =
           error.response && error.response.data.message
             ? error.response.data.message
             : error.message,
-      });
+      })
     }
-  };
+  }
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+    const { data } = await axios.get(`/api/products/${id}`)
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
