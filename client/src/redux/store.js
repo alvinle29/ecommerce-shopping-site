8cc97dba91909ee296dc6585ceb9aacab08a49f2
alvinle29 <bach938@gmail.com>
2022-10-21 16:08:45 +0300

    add login on redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,22 +1,31 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from "redux-thunk"
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
+import { userLoginReducer } from "./reducers/userReducer"
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : []
 
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromLocalStorage,
   }
 }
 
