89a038bc70c8dd09a8f9ae15c6ef069e3d906981
alvinle29 <bach938@gmail.com>
2022-10-21 23:33:59 +0300

    added redux user register

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from "redux-thunk"
 import { composeWithDevTools } from 'redux-devtools-extension'
+
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
-import { userLoginReducer } from "./reducers/userReducer"
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducer"
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-  userLogin: userLoginReducer
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
