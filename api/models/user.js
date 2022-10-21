50162b732ae0e2b4ab1a089eaf4a2d9d9637873e
alvinle29 <bach938@gmail.com>
2022-10-21 14:25:01 +0300

    regiter user added

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 import bcrypt from "bcryptjs"
 
 const userSchema = mongoose.Schema({
@@ -27,8 +27,17 @@ const userSchema = mongoose.Schema({
 )
 
 userSchema.methods.matchPassword = async function (enterPassword) {
-  return await bcrypt.compare(enterPassword, this.password);
-};
+  return await bcrypt.compare(enterPassword, this.password)
+}
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model("User", userSchema)
+
 export default User
--- api/router/userRouter.js
+++ api/router/userRouter.js
@@ -18,12 +18,45 @@ userRouter.post("/login", asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
       createdAt: user.createdAt,
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid Email or Password");
+    res.status(401)
+    throw new Error("Invalid Email or Password")
   }
 })
+)
+
+userRouter.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+      res.status(400)
+      throw new Error("User already exists")
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    })
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(400)
+      throw new Error("Invalid User Data")
+    }
+  })
 )
 
 userRouter.get('/profile', protect, asyncHandler(async (req, res) => {
@@ -36,20 +69,20 @@ userRouter.get('/profile', protect, asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       createdAt: user.createdAt,
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error("User not found.");
+    res.status(404)
+    throw new Error("User not found.")
   }
 }))
 
 userRouter.get("/:id", asyncHandler(async (req, res) => {
-  const product = await User.findById(req.params.id);
+  const product = await User.findById(req.params.id)
   if (product) {
-    res.json(product);
+    res.json(product)
   } else {
-    res.status(404);
-    throw new Error("User not Found");
+    res.status(404)
+    throw new Error("User not Found")
   }
 }))
 
