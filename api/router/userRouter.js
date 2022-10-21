c585ee6e22c3a9cacfcad28785c7328fe43b3eb5
alvinle29 <bach938@gmail.com>
2022-10-21 13:52:04 +0300

    user profile api added

--- api/router/userRouter.js
+++ api/router/userRouter.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
+import protect from "../middlewares/auth.js"
 import User from '../models/User.js'
 import generateToken from "../utils/generateTokens.js"
 
@@ -25,9 +26,21 @@ userRouter.post("/login", asyncHandler(async (req, res) => {
 })
 )
 
-userRouter.get('/', asyncHandler(async (req, res) => {
-  const products = await User.find({})
-  res.json(products)
+userRouter.get('/profile', protect, asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      createdAt: user.createdAt,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
 }))
 
 userRouter.get("/:id", asyncHandler(async (req, res) => {
