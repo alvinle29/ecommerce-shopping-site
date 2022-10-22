97923190c42812f26f61d8be07dc8885452f99e4
alvinle29 <bach938@gmail.com>
2022-10-22 15:29:25 +0300

    added product create, put, review to api

--- api/router/productRouter.js
+++ api/router/productRouter.js
@@ -10,13 +10,108 @@ productRouter.get('/', asyncHandler(async (req, res) => {
 }))
 
 productRouter.get("/:id", asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id)
+  
   if (product) {
-    res.json(product);
+    res.json(product)
   } else {
-    res.status(404);
-    throw new Error("Product not Found");
+    res.status(404)
+    throw new Error("Product not Found")
   }
 }))
+
+productRouter.post("/:id/review", protect, asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    )
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error("Product already Reviewed")
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    }
+
+    product.reviews.push(review)
+    product.numReviews = product.reviews.length
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
+
+    await product.save()
+    res.status(201).json({ message: "Reviewed Added" })
+  } else {
+    res.status(404)
+    throw new Error("Product not Found")
+  }
+})
+)
+
+productRouter.delete("/:id", protect, admin, asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    await product.remove()
+    res.json({ message: "Product deleted" })
+  } else {
+    res.status(404)
+    throw new Error("Product not Found")
+  }
+})
+)
+
+productRouter.post("/", protect, admin, asyncHandler(async (req, res) => {
+  const { name, price, description, image, countInStock } = req.body
+  const productExist = await Product.findOne({ name })
+
+  if (productExist) {
+    res.status(400)
+    throw new Error("Product name already exist")
+  } else {
+    const product = new Product({
+      name,
+      price,
+      description,
+      image,
+      countInStock,
+      user: req.user._id,
+    })
+    if (product) {
+      const createdproduct = await product.save()
+      res.status(201).json(createdproduct)
+    } else {
+      res.status(400)
+      throw new Error("Invalid product data")
+    }
+  }
+})
+)
+
+productRouter.put("/:id", protect, admin, asyncHandler(async (req, res) => {
+  const { name, price, description, image, countInStock } = req.body
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name || product.name
+    product.price = price || product.price
+    product.description = description || product.description
+    product.image = image || product.image
+    product.countInStock = countInStock || product.countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error("Product not found")
+  }
+})
+)
 
 export default productRouter
