9bf533a1c63244da4be1fc8e3f2f762e5787b15e
alvinle29 <bach938@gmail.com>
2022-10-22 13:04:27 +0300

    added redux for shipping

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -24,13 +24,18 @@ const userInfoFromLocalStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null
 
+const shippingAddressFromLocalStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
+    shippingAddress: shippingAddressFromLocalStorage,
   },
   userLogin: {
     userInfo: userInfoFromLocalStorage,
-  }
+  },
 }
 
 const middleware = [thunk]
