8149262b342396d95a6bb19fcb3e3c1eaf4f633b
alvinle29 <bach938@gmail.com>
2022-10-25 10:48:22 +0300

    fix api to deploy

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
