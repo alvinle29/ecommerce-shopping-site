63bd1c42034fdb32f7d192d403a304fc5cd60d71
alvinle29 <bach938@gmail.com>
2022-12-07 01:30:29 +0200

    added cors

--- api/index.js
+++ api/index.js
@@ -15,6 +15,8 @@ connectDatabase()
 const app = express()
 app.use(express.json())
 
+app.use(cors())
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.header(
